28853da04606c2fbd24dbe57547f7547b2146d3c
Pierre <pierre.laffaille@outlook.fr>
2025-09-24 15:54:57 +0200

    complete dijkstra, returns path set

--- dijsktra.ts
+++ dijsktra.ts
@@ -1,6 +1,10 @@
-export default function dijkstra(graph: Object, start: String) {
-	const distances = {};
+function dijkstra(
+	graph: { [key: string]: { [key: string]: number } },
+	start: string
+): {} {
+	const distances: { [key: string]: number } = {};
 	const visited = new Set();
+	const path = new Set();
 	const queue = [start];
 
 	for (let node in graph) {
@@ -10,7 +14,7 @@ export default function dijkstra(graph: Object, start: String) {
 	distances[start] = 0;
 
 	while (queue.length > 0) {
-		let currentNode = queue.shift();
+		let currentNode: any = queue.shift();
 		if (visited.has(currentNode)) continue;
 		visited.add(currentNode);
 		let neighbors = graph[currentNode];
@@ -18,9 +22,24 @@ export default function dijkstra(graph: Object, start: String) {
 			let newDist = distances[currentNode] + neighbors[neighbor];
 			if (newDist < distances[neighbor]) {
 				distances[neighbor] = newDist;
+				path.add(currentNode);
 				queue.push(neighbor);
 			}
 		}
 	}
-	return distances;
+	return path;
 }
+
+const graph = {
+	A: { B: 1, C: 2 },
+	B: { A: 1, C: 2, D: 2, E: 3 },
+	C: { A: 2, B: 2, D: 1 },
+	D: { B: 2, C: 1, E: 3, F: 4 },
+	E: { B: 3, D: 3, G: 3 },
+	F: { D: 4, G: 1 },
+	G: { E: 3, F: 1 },
+};
+
+console.log(dijkstra(graph, "A"));
+
+export default dijkstra;
